054a07272f1afb81a89ff3e67dfcbcd916774c0d
Chris Speck <cgspeck@gmail.com>
2021-10-15 14:47:41 +1100

    io, loops, matching

---
+++ ch02/guessing_game/src/main.rs
@@ -0,0 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Input your guess:");
+
+        let mut guess = String::new();
+
+        let read_bytes = io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Read {} bytes", read_bytes);
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
